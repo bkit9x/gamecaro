92af59efc35aac7cddfe88515580ba155d4c2dab
hkit9x <bkit9x@gmail.com>
2021-08-19 17:40:57 +0700

    hoan thanh v1

--- game.js
+++ game.js
@@ -1,48 +1,104 @@
 class Game {
-    constructor(row, col, user, dbRoom) {
-        this.row = row
-        this.col = col
-        this.user = user
-        this.dbRoom = dbRoom
-        this.resetGame()
-        document.getElementById('reset').onclick = this.resetGame.bind(this)
 
-        dbRoom.on('value', (snapshot) => {
+    constructor() {
+        this.row = 20
+        this.col = 20
+        this.result = document.getElementById('result')
+        this.listRoom = document.getElementById('listRoom')
+        document.getElementById('msg').addEventListener("keyup", function (event) {
+            if (event.keyCode === 13) {
+                event.preventDefault();
+                document.getElementById("send").click();
+            }
+        })
+        var config = {
+            apiKey: "AIzaSyCPMNowtEiT0Z3tSFoZwkAOE6KgnkCzNkE",
+            authDomain: "caro-5c82f.firebaseapp.com.firebaseapp.com",
+            databaseURL: "https://caro-5c82f-default-rtdb.asia-southeast1.firebasedatabase.app",
+            projectId: "caro-5c82f",
+            storageBucket: "caro-5c82f.appspot.com",
+        }
+        firebase.initializeApp(config)
+        this.initUser()
+
+        this.db = firebase.database().ref()
+        this.dbRoom = this.db.child('room1')
+        this.dbChat = this.db.child('chat1')
+        document.getElementById('reset').onclick = this.resetGame.bind(this)
+        document.getElementById('send').onclick = this.sendMessage.bind(this, this)
+
+
+        this.resetGame()
+        this.dbRoom.on('value', (snapshot) => {
             const data = snapshot.val();
-            if (data == null) {
+            if (data.data == "null") {
                 this.resetGame()
             }
             else {
-                this.setData(JSON.parse(data.data))
-                this.setTurn(data.turn)
-                if (data.lastUser != null) {
-                    this.markCell(document.getElementById("cell_" + data.lastRow + "_" + data.lastCol), data.lastUser)
+                try {
+                    this.setData(JSON.parse(data.data))
+                    this.setTurn(data.turn)
+                    if (data.lastUser != null) {
+                        this.markCell(document.getElementById("cell_" + data.lastRow + "_" + data.lastCol), data.lastUser)
+                    }
+                    if (data.win != null) {
+                        if (data.win == this.user)
+                            var win = "Thắng rồi! "
+                        else
+                            var win = "Thua rồi... :("
+                        alert(win)
+                        this.result.innerText = win
+                        this.result.classList.remove('hidden')
+                    }
                 }
-                if (data.win != null) {
-                    if (data.win == this.user)
-                        var win = "Chúc mừng bạn đã thắng! :)"
-                    else
-                        var win = "Thua rồi... :("
-                    document.getElementById('result').innerText = win
-                }
-
+                catch (e) { }
             }
-
         });
 
+        // .orderByKey()
+        this.dbChat.on("value", function (snapshot) {
+            snapshot.forEach(function (childSnapshot) {
+                // var key = childSnapshot.key;
+                var chatHTML = document.getElementById('chat')
+                var childData = childSnapshot.val();
+                var node = document.createElement("DIV")
+                var textnode = document.createTextNode(childData)
+                node.appendChild(textnode)
+
+                chatHTML.appendChild(node)
+            });
+        }, function (error) {
+            console.error(error);
+        });
+    }
+
+    initUser() {
+        if (typeof (Storage) !== "undefined") {
+            if (localStorage.getItem("user") == "null") {
+                this.user = prompt('nhập x hoặc o')
+                localStorage.setItem("user", this.user)
+            }
+            else {
+                this.user = localStorage.getItem("user")
+
+            }
+        } else {
+            var a = prompt('nhập x hoặc o')
+            this.user = a
+        }
     }
 
     resetGame() {
         console.log("reset")
+        this.dbRoom.set({ data: "null" })
         this.createData(this.row, this.col)
         this.createBroad(this.row, this.col)
-        this.dbRoom.set({ data: JSON.stringify(this.data), turn: 'x' })
-        this.turn = 'x'
+        this.setTurn('x')
         var cell = document.getElementsByClassName('cell')
         for (var i = 0; i < cell.length; i++) {
             cell[i].onclick = this.checkCell.bind(this, cell[i])
         }
-
+        this.result.classList.add('hidden')
     }
 
     markCell(cell, user) {
@@ -65,16 +121,12 @@ class Game {
         let col = Number(cell.getAttribute("data-col"));
         if (this.turn == this.user && this.data[row][col] == null) {
             this.markCell(cell, this.user)
-
-
-            var end = this.win(row, col, this.user)
-            if (end) {
-                alert(user + " Thắng! ")
-                dbRoom.set({ data: JSON.stringify(this.data), turn: this.turn, lastRow: row, lastCol: col, lastUser: this.user, win: this.user })
+            var win = this.win(row, col, this.user)
+            var data = { data: JSON.stringify(this.data), turn: this.turn, lastRow: row, lastCol: col, lastUser: this.user, time: new Date().getTime() }
+            if (win) {
+                data.win = this.user
             }
-            else {
-                dbRoom.set({ data: JSON.stringify(this.data), turn: this.turn, lastRow: row, lastCol: col, lastUser: this.user })
-            }
+            this.dbRoom.set(data)
         }
     }
 
@@ -131,24 +183,9 @@ class Game {
             }
         }
 
-        
-        for (let i = -4; i <= 4; i++) {
-            let count = 0;
-            for (let j = 0; j < 5; j++) {
-                if ((col + i) >= 0 && (col + i + j) < this.col && (row + i) >= 0 && (row + i + j) < this.row) {
-                    console.log("i,j, row, col", i,j, row + i, col + 8 - i - j)
-                    if (this.data[row + i][col + 8 - i - j] == x) {
-                        count++;
-                    }
-                }
-            }
-            if (count == 5) {
-                return true;
-            }
-        }
-
         return null;
     }
+
     //tạo mảng lưu các nước đi
     createData(row, col) {
         var data = []
@@ -185,6 +222,10 @@ class Game {
             var turnof = "Chờ xíu nhé!"
         document.getElementById('turnof').innerText = turnof
     }
+    setRoom(room) {
+        this.room = room
+    }
+
 
     getData(row, col) {
         if (this.data[row])
@@ -192,4 +233,19 @@ class Game {
                 return this.data[row][col]
         return undefined
     }
+
+    sendMessage(game) {
+        console.log(game)
+        var msg = document.getElementById('msg')
+        if (msg.value != "") {
+            var time = new Date().getTime();
+            var data = new Object()
+            data[time] = game.user + ": " + msg.value
+            game.dbChat.set(data)
+            msg.value = ""
+            document.getElementById("chat").scrollTop = document.getElementById("chat").scrollHeight
+        }
+    }
+
+
 }
